f5286be1d74ad21f33d9759c31c7c4a26d90eafb
Julián Campos <m02capej@uco.es>
2022-05-23 22:41:09 +0200

    add edit text to set timer

--- app/src/main/java/com/codeandhacks/chesstimer/SetTimer.kt
+++ app/src/main/java/com/codeandhacks/chesstimer/SetTimer.kt
@@ -1,11 +1,30 @@
 package com.codeandhacks.chesstimer
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.codeandhacks.chesstimer.databinding.ActivitySetTimerBinding
 
 class SetTimer : AppCompatActivity() {
+    private lateinit var binding : ActivitySetTimerBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_set_timer)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding = ActivitySetTimerBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        binding.setTimerBtnBack.setOnClickListener{
+            val intent = Intent(this, Home::class.java)
+            startActivity(intent)
+
+            //putExtras
+
+            finish()
+        }
     }
 }
