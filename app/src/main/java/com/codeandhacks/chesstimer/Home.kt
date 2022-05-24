019db38aa34a07bfb00bc78aa37d1e92dbdde484
Julián Campos <m02capej@uco.es>
2022-05-24 13:23:19 +0200

    countdowns added

--- app/src/main/java/com/codeandhacks/chesstimer/Home.kt
+++ app/src/main/java/com/codeandhacks/chesstimer/Home.kt
@@ -8,10 +8,22 @@ import android.os.Bundle
 import android.os.CountDownTimer
 import android.util.Log
 import com.codeandhacks.chesstimer.databinding.ActivityHomeBinding
+import java.sql.Time
+import java.util.concurrent.TimeUnit
 
 class Home : AppCompatActivity() {
     private lateinit var binding : ActivityHomeBinding
     private val TAG = "Reloj"
+    private lateinit var timerPlayer1 : CountDownTimer
+    private lateinit var timerPlayer2 : CountDownTimer
+    private var hoursPlayer1 = -1L
+    private var minutesPlayer1 = -1L
+    private var secondsPlayer1 = -1L
+    private var hoursPlayer1Milis = -1L
+    private var minutesPlayer1Milis = -1L
+    private var secondsPlayer1Milis = -1L
+    private var timePlayer1Milis = -1L
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +39,7 @@ class Home : AppCompatActivity() {
 
         //Set the buttons color at the beggining
         binding.homeBtnPlayer2.setBackgroundColor(Color.GRAY)
-        binding.homeBtnPlayer1.setBackgroundColor(Color.CYAN)
+        binding.homeBtnPlayer1.setBackgroundColor(Color.parseColor("#00b084"))
 
 
         //Configure the timer
@@ -36,10 +48,84 @@ class Home : AppCompatActivity() {
             startActivity(intent)
         }
 
-
         var status = 3
-        var timePlayer1 = "3:00"
-        var timePlayer2 = "3:00"
+
+        hoursPlayer1 = (0 * 60 * 60).toLong()
+        minutesPlayer1 = (1 * 60).toLong()
+        secondsPlayer1 = (0).toLong()
+        hoursPlayer1Milis = hoursPlayer1*1000
+        minutesPlayer1Milis = minutesPlayer1*1000
+        secondsPlayer1Milis = secondsPlayer1*1000
+        timePlayer1Milis  = hoursPlayer1Milis+minutesPlayer1Milis+secondsPlayer1Milis
+
+        if(hoursPlayer1 == 0L && minutesPlayer1/60 >= 10){
+            binding.homeBtnPlayer1.text = "${minutesPlayer1/60}:$secondsPlayer1"
+            binding.homeBtnPlayer1.setTextColor(Color.WHITE)
+        }
+
+        if(hoursPlayer1 == 0L && minutesPlayer1/60 < 10){
+            binding.homeBtnPlayer1.text = "0${minutesPlayer1/60}:$secondsPlayer1"
+            binding.homeBtnPlayer1.setTextColor(Color.WHITE)
+        }
+
+        if(hoursPlayer1 == 0L && minutesPlayer1/60 < 10 && secondsPlayer1 < 10){
+            binding.homeBtnPlayer1.text = "0${minutesPlayer1/60}:0$secondsPlayer1"
+            binding.homeBtnPlayer1.setTextColor(Color.WHITE)
+        }
+
+        if(hoursPlayer1/3600 == 1L && minutesPlayer1/60 >= 10){
+            binding.homeBtnPlayer1.text = "${hoursPlayer1/3600}.${minutesPlayer1/60}:$secondsPlayer1"
+            binding.homeBtnPlayer1.setTextColor(Color.WHITE)
+        }
+
+        if(hoursPlayer1/3600 == 1L && minutesPlayer1/60 < 10){
+            binding.homeBtnPlayer1.text = "${hoursPlayer1/3600}.0${minutesPlayer1/60}:$secondsPlayer1"
+            binding.homeBtnPlayer1.setTextColor(Color.WHITE)
+        }
+
+        if(hoursPlayer1/3600 == 1L && minutesPlayer1/60 < 10 && secondsPlayer1 < 10){
+            binding.homeBtnPlayer1.text = "${hoursPlayer1/3600}.0${minutesPlayer1/60}:0$secondsPlayer1"
+            binding.homeBtnPlayer1.setTextColor(Color.WHITE)
+        }
+
+        var hoursPlayer2 = (0 * 60 * 60).toLong()
+        var minutesPlayer2 = (1 * 60).toLong()
+        var secondsPlayer2 = (0).toLong()
+        var hoursPlayer2milis = hoursPlayer2*1000
+        var minutesPlayer2milis = minutesPlayer2*1000
+        var secondsPlayer2milis = secondsPlayer2*1000
+        var timePlayer2milis  = hoursPlayer2milis+minutesPlayer2milis+secondsPlayer2milis
+
+        if(hoursPlayer2 == 0L && minutesPlayer2/60 >= 10){
+            binding.homeBtnPlayer2.text = "${minutesPlayer2/60}:$secondsPlayer2"
+            binding.homeBtnPlayer2.setTextColor(Color.WHITE)
+        }
+
+        if(hoursPlayer2 == 0L && minutesPlayer2/60 < 10){
+            binding.homeBtnPlayer2.text = "0${minutesPlayer2/60}:$secondsPlayer2"
+            binding.homeBtnPlayer2.setTextColor(Color.WHITE)
+        }
+
+        if(hoursPlayer2 == 0L && minutesPlayer2/60 < 10 && secondsPlayer2 < 10){
+            binding.homeBtnPlayer2.text = "0${minutesPlayer2/60}:0$secondsPlayer2"
+            binding.homeBtnPlayer2.setTextColor(Color.WHITE)
+        }
+
+        if(hoursPlayer2/3600 == 1L && minutesPlayer2/60 >= 10){
+            binding.homeBtnPlayer2.text = "${hoursPlayer1/3600}.${minutesPlayer2/60}:$secondsPlayer2"
+            binding.homeBtnPlayer2.setTextColor(Color.WHITE)
+        }
+
+        if(hoursPlayer2/3600 == 1L && minutesPlayer2/60 < 10){
+            binding.homeBtnPlayer2.text = "${hoursPlayer1/3600}.0${minutesPlayer2/60}:$secondsPlayer2"
+            binding.homeBtnPlayer2.setTextColor(Color.WHITE)
+        }
+
+        if(hoursPlayer2/3600 == 1L && minutesPlayer2/60 < 10 && secondsPlayer2 < 10){
+            binding.homeBtnPlayer2.text = "${hoursPlayer1/3600}.0${minutesPlayer2/60}:0$secondsPlayer2"
+            binding.homeBtnPlayer2.setTextColor(Color.WHITE)
+        }
+
 
 
         //Show the timer
@@ -50,37 +136,178 @@ class Home : AppCompatActivity() {
             //Buttons are available
             status = 0
             binding.homeBtnPlayer2.setBackgroundColor(Color.GRAY)
-            binding.homeBtnPlayer1.setBackgroundColor(Color.CYAN)
+            binding.homeBtnPlayer1.setBackgroundColor(Color.parseColor("#00b084"))
         }
 
         binding.homeBtnPlayer1.setOnClickListener {
-            if (status == 1 || status == 0) {
+            if (status == 0) {
                 //Change the colours when is enabled and not
                 binding.homeBtnPlayer1.setBackgroundColor(Color.GRAY)
-                binding.homeBtnPlayer2.setBackgroundColor(Color.CYAN)
-
-                //Stop the player 1 timer
+                binding.homeBtnPlayer2.setBackgroundColor(Color.parseColor("#00b084"))
 
                 //Start the player 2 timer
+                timerPlayer2 = object : CountDownTimer(timePlayer2milis, 100){
+                    override fun onTick(p0: Long) {
 
+                        var timeHours = p0 / 60 /60 / 1000
+                        var timeMinutes = p0 / 60 / 1000 % 60
+                        var timeSeconds = p0 / 1000 % 60
+
+
+                        if(timeHours > 0 && timeMinutes > 10){
+                            var timeToShow = "$timeHours.$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer2.text= timeToShow
+                        } else if (timeHours > 0 && timeMinutes < 10) {
+                            var timeToShow = "$timeHours.0$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer2.text= timeToShow
+                        } else if (timeHours < 1 && timeMinutes < 10) {
+                            var timeToShow = "0$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer2.text= timeToShow
+                        } else {
+                            var timeToShow = "$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer2.text = timeToShow
+                        }
+                        binding.homeBtnPlayer2.setTextColor(Color.WHITE)
+
+
+                    }
+                    override fun onFinish() {
+                        binding.homeBtnPlayer2.setBackgroundColor(Color.RED)
+                    }
+
+                }.start()
+                //Change the status
+                status = 2
+            }
+
+            if (status == 1) {
+                //Change the colours when is enabled and not
+                binding.homeBtnPlayer1.setBackgroundColor(Color.GRAY)
+                binding.homeBtnPlayer2.setBackgroundColor(Color.parseColor("#00b084"))
+
+                //Stop player1Timer
+
+                timerPlayer1.cancel()
+
+                //Start the player 2 timer
+                timerPlayer2 = object : CountDownTimer(timePlayer2milis, 100){
+                    override fun onTick(p0: Long) {
+
+                        var timeHours = p0 / 60 /60 / 1000
+                        var timeMinutes = p0 / 60 / 1000 % 60
+                        var timeSeconds = p0 / 1000 % 60
+
+                        if(timeHours > 0 && timeMinutes > 10){
+                            var timeToShow = "$timeHours.$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer2.text= timeToShow
+                        } else if (timeHours > 0 && timeMinutes < 10) {
+                            var timeToShow = "$timeHours.0$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer2.text= timeToShow
+                        } else if (timeHours < 1 && timeMinutes < 10) {
+                            var timeToShow = "0$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer2.text= timeToShow
+                        } else {
+                            var timeToShow = "$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer2.text = timeToShow
+                        }
+
+                        binding.homeBtnPlayer2.setTextColor(Color.WHITE)
+
+                    }
+                    override fun onFinish() {
+                        TODO("Not yet implemented")
+                    }
+
+                }.start()
                 //Change the status
                 status = 2
             }
         }
 
         binding.homeBtnPlayer2.setOnClickListener {
-            if (status == 2 || status == 0) {
+            if (status == 0) {
                 //Change the colours when is enabled and not
                 binding.homeBtnPlayer2.setBackgroundColor(Color.GRAY)
-                binding.homeBtnPlayer1.setBackgroundColor(Color.CYAN)
-
-                //Stop the player 2 timer
+                binding.homeBtnPlayer1.setBackgroundColor(Color.parseColor("#00b084"))
 
                 //Start the player 1 timer
+                timerPlayer1 = object : CountDownTimer(timePlayer1Milis, 100){
+                    override fun onTick(p0: Long) {
 
+                        var timeHours = p0 / 60 /60 / 1000
+                        var timeMinutes = p0 / 60 / 1000 % 60
+                        var timeSeconds = p0 / 1000 % 60
+
+                        if(timeHours > 0 && timeMinutes > 10){
+                            var timeToShow = "$timeHours.$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer1.text= timeToShow
+                        } else if (timeHours > 0 && timeMinutes < 10) {
+                            var timeToShow = "$timeHours.0$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer1.text= timeToShow
+                        } else if (timeHours < 1 && timeMinutes < 10) {
+                            var timeToShow = "0$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer1.text= timeToShow
+                        } else {
+                            var timeToShow = "$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer1.text = timeToShow
+                        }
+                        binding.homeBtnPlayer1.setTextColor(Color.WHITE)
+
+                    }
+                    override fun onFinish() {
+                        TODO("Not yet implemented")
+                    }
+
+                }.start()
                 //Change the status
                 status = 1
             }
+
+            if (status == 2) {
+                //Change the colours when is enabled and not
+                binding.homeBtnPlayer2.setBackgroundColor(Color.GRAY)
+                binding.homeBtnPlayer1.setBackgroundColor(Color.parseColor("#00b084"))
+
+                //Stop the player 2 timer
+                timerPlayer2.cancel()
+
+                //Start the player 1 timer
+
+                timerPlayer1 = object : CountDownTimer(timePlayer1Milis, 100){
+                    override fun onTick(p0: Long) {
+
+                        var timeHours = p0 / 60 /60 / 1000
+                        var timeMinutes = p0 / 60 / 1000 % 60
+                        var timeSeconds = p0 / 1000 % 60
+
+                        if(timeHours > 0 && timeMinutes > 10){
+                            var timeToShow = "$timeHours.$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer1.text= timeToShow
+                        } else if (timeHours > 0 && timeMinutes < 10) {
+                            var timeToShow = "$timeHours.0$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer1.text= timeToShow
+                        } else if (timeHours < 1 && timeMinutes < 10) {
+                            var timeToShow = "0$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer1.text= timeToShow
+                        } else {
+                            var timeToShow = "$timeMinutes:$timeSeconds"
+                            binding.homeBtnPlayer1.text = timeToShow
+                        }
+                        binding.homeBtnPlayer1.setTextColor(Color.WHITE)
+
+                    }
+                    override fun onFinish() {
+                        TODO("Not yet implemented")
+                    }
+
+                }.start()
+                //Change the status
+                status = 1
+            }
+        }
+
+        binding.homeBtnPauseTimer.setOnClickListener {
+            status = 3
         }
 
     }
--- app/src/main/java/com/codeandhacks/chesstimer/SetTimer.kt
+++ app/src/main/java/com/codeandhacks/chesstimer/SetTimer.kt
@@ -3,6 +3,7 @@ package com.codeandhacks.chesstimer
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.codeandhacks.chesstimer.databinding.ActivitySetTimerBinding
 
 class SetTimer : AppCompatActivity() {
@@ -16,15 +17,170 @@ class SetTimer : AppCompatActivity() {
         super.onResume()
         binding = ActivitySetTimerBinding.inflate(layoutInflater)
         val view = binding.root
+        val TAG = "Set"
         setContentView(view)
+
+        //Time from ET
+
+        var hours = 0
+        var minutes = 5
+        var seconds = 0
+        var increment = 0
+
+        binding.setTimerHours.setText(hours.toString())
+        binding.setTimerMinutes.setText("0${minutes.toString()}")
+        binding.setTimerSeconds.setText("0${seconds.toString()}")
+
+        //Arrow buttons
+
+        binding.setTimerBtnHoursUp.setOnClickListener{
+            hours += 1
+            binding.setTimerHours.setText(hours.toString())
+        }
+
+        binding.setTimerBtnHoursDown.setOnClickListener {
+            if(hours > 0){
+                hours -= 1
+                binding.setTimerHours.setText(hours.toString())
+            }
+        }
+
+        binding.setTimerBtnMinutesUp.setOnClickListener {
+            if(minutes < 60){
+                minutes += 1
+            }
+            if(minutes < 10){
+                binding.setTimerMinutes.setText("0${minutes.toString()}")
+            } else {
+                binding.setTimerMinutes.setText(minutes.toString())
+            }
+        }
+
+        binding.setTimerBtnMinutesDown.setOnClickListener {
+            if(minutes > 0){
+                minutes -= 1
+            }
+            if(minutes < 10){
+                binding.setTimerMinutes.setText("0${minutes.toString()}")
+            } else {
+                binding.setTimerMinutes.setText(minutes.toString())
+            }
+        }
+
+        binding.setTimerBtnSecondsUp.setOnClickListener {
+            if(seconds < 60){
+                seconds += 1
+            }
+            if(seconds < 10){
+                binding.setTimerSeconds.setText("0${seconds.toString()}")
+            } else {
+                binding.setTimerSeconds.setText(seconds.toString())
+            }
+        }
+
+        binding.setTimerBtnSecondsDown.setOnClickListener {
+            if(seconds > 0){
+                seconds -= 1
+            }
+
+            if(seconds < 10){
+                binding.setTimerSeconds.setText("0${seconds.toString()}")
+            } else {
+                binding.setTimerSeconds.setText(seconds.toString())
+            }
+
+        }
+
+        //Buttons with time
+
+        binding.setTimerBtn1.setOnClickListener {
+            hours = 0
+            minutes = 1
+            seconds = 0
+            binding.setTimerHours.setText(hours.toString())
+            binding.setTimerMinutes.setText("0${minutes.toString()}")
+            binding.setTimerSeconds.setText("0${seconds.toString()}")
+        }
+
+        binding.setTimerBtn2.setOnClickListener {
+            hours = 0
+            minutes = 2
+            seconds = 0
+            binding.setTimerHours.setText(hours.toString())
+            binding.setTimerMinutes.setText("0${minutes.toString()}")
+            binding.setTimerSeconds.setText("0${seconds.toString()}")
+        }
+
+        binding.setTimerBtn3.setOnClickListener {
+            hours = 0
+            minutes = 3
+            seconds = 0
+
+            binding.setTimerHours.setText(hours.toString())
+            binding.setTimerMinutes.setText("0${minutes.toString()}")
+            binding.setTimerSeconds.setText("0${seconds.toString()}")
+        }
+
+        binding.setTimerBtn4.setOnClickListener {
+            hours = 0
+            minutes = 5
+            seconds = 0
+            binding.setTimerHours.setText(hours.toString())
+            binding.setTimerMinutes.setText("0${minutes.toString()}")
+            binding.setTimerSeconds.setText("0${seconds.toString()}")
+        }
+
+        binding.setTimerBtn5.setOnClickListener {
+            hours = 0
+            minutes = 10
+            seconds = 0
+
+            binding.setTimerHours.setText(hours.toString())
+            binding.setTimerMinutes.setText(minutes.toString())
+            binding.setTimerSeconds.setText("0${seconds.toString()}")
+        }
+
+        binding.setTimerBtn6.setOnClickListener {
+            hours = 0
+            minutes = 15
+            seconds = 0
+
+            binding.setTimerHours.setText(hours.toString())
+            binding.setTimerMinutes.setText(minutes.toString())
+            binding.setTimerSeconds.setText("0${seconds.toString()}")
+        }
+
+        binding.setTimerSelectorIncrement.setOnClickListener {
+
+            if(binding.setTimerSelectorIncrement.isChecked){
+                //Alert dialog to chose the increment
+
+            }
+
+            if(binding.setTimerSelectorIncrement.isChecked == false){
+                increment = 0
+            }
+        }
 
         binding.setTimerBtnBack.setOnClickListener{
             val intent = Intent(this, Home::class.java)
             startActivity(intent)
-
             //putExtras
 
             finish()
+        }
+
+        binding.setTimerBtnSave.setOnClickListener {
+            val hoursToSave = binding.setTimerHours.toString()
+            val minutesToSave = binding.setTimerMinutes.toString()
+            val secondsToSave = binding.setTimerSeconds.toString()
+
+            val intent = Intent(this, Home::class.java)
+            intent.putExtra("Hours", hoursToSave)
+            intent.putExtra("Minutes", minutesToSave)
+            intent.putExtra("Seconds", secondsToSave)
+            intent.putExtra("Increment", increment)
+            startActivity(intent)
         }
     }
 }
